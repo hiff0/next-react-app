3b847c396bac4d7a207e8477c7a94d78f64f48cc
Петров Владислав <vapetrov@nic.etu>
2023-06-23 16:45:03 +0300

    project init

---
+++ src/app/components/Films.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export const Films = () => {
+    return (
+        <div>
+            <span>Оно</span>
+            <br />
+            <span>Оно 2</span>
+        </div>
+    )
+}
